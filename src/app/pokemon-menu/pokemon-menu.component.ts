9df16fc81177ef86569c6161c7f0c8723b604538
hnunezz <nunesdealmeidahenrique@gmail.com>
2021-12-24 15:30:11 -0300

    feat: social media button

--- src/app/pokemon-menu/pokemon-menu.component.ts
+++ src/app/pokemon-menu/pokemon-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-menu',
@@ -8,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class PokemonMenuComponent implements OnInit {
 
   public aberto:boolean = false;
-  constructor() { }
+  public url_1:string = 'https://github.com/hnunezz';
+  public url_2:string = 'https://www.linkedin.com/in/henriquenunes27/';
+
+  constructor( @Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
+  }
+
+
+  goToGithub(url_1: string): void {
+     window.open(this.url_1,"_blank");
+  }
+
+  goToLinkedin(url_2:string): void {
+     window.open(this.url_2,"_blank");
   }
 
 }
